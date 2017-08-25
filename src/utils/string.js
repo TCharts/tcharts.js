7876c42fa420271f0151c35391d8731e5069196f
Pandorym <Pandorym@Foxmail.com>
2017-08-25 08:37:12 +0800

    Add chinese support
    The width of a Chinese is equal to two in English.

--- src/utils/string.js
+++ src/utils/string.js
@@ -3,7 +3,7 @@
  * Contract: i@hust.cc
  */
 
-const wordWidth = text => (text ? text.length : 0);
+const wordWidth = text => (text ? text.length + (`${text}中`.match(/[^\x00-\x80]/g).length - 1) : 0);
 
 module.exports = {
   wordWidth,
